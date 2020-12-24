1f7b302dd1b551b424c258ba84d2ff75a968eb5f
wei <503945930@qq.com>
2020-12-24 11:23:54 +0800

    pm2 config

---
+++ sync/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+        name: "near-sync",
+        script: "./index.js",
+        env: {
+
+        },
+        env_production: {
+
+        },
+        "out_file": "/dev/null",
+        "error_file": "/dev/null"
+    }]
+}
